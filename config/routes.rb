9b969cc5f984a2859f77972af70bda5d6e923b38
Luke Busby <labusby1@gmail.com>
2018-12-15 19:30:32 +0000

    Clened up contact routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
-  resources :contacts
-  get 'contact-us', to: 'contacts#new'
+  resources :contacts, only: :create #We create 7 possible actions, but then specify, not all 7, just new
+  get 'contact-us', to: 'contacts#new', as: 'new_contact'
 end
